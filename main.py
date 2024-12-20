6ee8fd7e8f22821342340d558d7934be5d40d9cf
eliseeva205s9 <veliseeva006@mail.ru>
2024-12-20 22:29:42 +0700

    main.py

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
-@@ -0,0 +1,41 @@
+@@ -0,0 +1,18 @@
 
-from flask import Flask, request, jsonifyapp = Flask(__name__)
-@app.route('/calculate/<num1>/<num2>/<operation>')
-def calculate(num1, num2, operation):
-    """
-    Обрабатывает GET-запрос с данными для вычисления.
-    Args:
-        num1 (str): Первое число.
-        num2 (str): Второе число.
-        operation (str): Операция ("add", "subtract", "multiply", "divide").
-    Returns:
-        JSON-ответ с результатом вычисления или ошибкой.
-    """
-    try:
-        num1 = float(num1)
-        num2 = float(num2)
-        if operation == 'add':
-            result = num1 + num2
-        elif operation == 'subtract':
-            result = num1 - num2
-        elif operation == 'multiply':
-            result = num1 * num2
-        elif operation == 'divide':
-            if num2 == 0:
-                return jsonify({'error': 'Деление на ноль недопустимо'}), 400
-            result = num1 / num2
-        else:
-            return jsonify({'error': 'Неверная операция'}), 400
-        return jsonify({'result': result})
-    except ValueError as e:
-        return jsonify({'error': f'Некорректный ввод данных: {e}'}), 400
-if name == '__main__':
-    app.run(debug=True)
+import requestsdef calculate_and_print():
+    """Запрашивает у пользователя ссылку, отправляет GET-запрос и выводит результат."""
+    num1 = input("Введите число 1")
+    num2 = input("Введите число 2")
+    operation = input("weweweewerrrer")
+    response = requests.get("http://127.0.0.1:5000/calculate/"+num1+"/"+num2+"/"+operation )
+    if response.status_code == 200:
+        result = response.json()['result']
+        print(f"Результат: {result}")
+    else:
+        print(f"Ошибка: {response.text}")
+if name == "__main__":
+    calculate_and_print()
