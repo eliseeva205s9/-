9dfc16415f1c6c86d0605e463b87584d4bdf9b07
eliseeva205s9 <veliseeva006@mail.ru>
2024-12-20 22:31:02 +0700

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-@@ -0,0 +1,18 @@
+import requests
 
-import requestsdef calculate_and_print():
+def calculate_and_print():
     """Запрашивает у пользователя ссылку, отправляет GET-запрос и выводит результат."""
     num1 = input("Введите число 1")
     num2 = input("Введите число 2")
     operation = input("weweweewerrrer")
+
     response = requests.get("http://127.0.0.1:5000/calculate/"+num1+"/"+num2+"/"+operation )
+
     if response.status_code == 200:
         result = response.json()['result']
         print(f"Результат: {result}")
     else:
         print(f"Ошибка: {response.text}")
-if name == "__main__":
+
+if __name__ == "__main__":
     calculate_and_print()
